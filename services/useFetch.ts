60752c34aecf428b962241172c9d8d882d9556ab
Seb <sebastian.muriel01@gmail.com>
2025-06-12 08:53:31 -0700

    API response successful, UI not showing it

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -4,40 +4,42 @@
 
 // useFetch(fetchMovies)
 
-import React from "react";
+import { useState, useEffect } from "react";
 
 const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
-    const [data, setData] = React.useState<T | null>(null);
-    const [loading, setLoading] = React.useState(false);
-    const [error, setError] = React.useState<Error | null>(null);
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
-    const fetchData = async () => {
-        try {
-            setLoading(true);
-            setError(null);
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
 
-            const result = await fetchFunction()
-            setData(result);
-
-        } catch (err) {
-            // @ts-ignore
-            setError(err instanceof Error ? err: new Error('An error occurred.'))
-        } finally {
-            setLoading(false);
-        }
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+      setError(
+        err instanceof Error ? err : new Error("An unknown error occurred")
+      );
+    } finally {
+      setLoading(false);
     }
-    const reset = () => {
-        setData(null);
-        setLoading(false);
-        setError(null);
-    }
-    React.useEffect(() => {
-        if (autoFetch) {
-            fetchData();
-        }
+  };
 
-    }, []);
-    return { data, loading, error, refetch: fetchData, reset } 
-}
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, refetch: fetchData, reset };
+};
 
 export default useFetch;
